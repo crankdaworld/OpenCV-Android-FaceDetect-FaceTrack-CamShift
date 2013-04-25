dac94610d2e0c744e6417cb1908f3f54087978aa
Imran Akthar <imranakthar@gmail.com>
2013-04-25 18:13:12 +0530

    Update Source4minternet.java

--- Project1/face-detection/src/org/opencv/samples/fd/Source4minternet.java
+++ Project1/face-detection/src/org/opencv/samples/fd/Source4minternet.java
@@ -1,5 +1,8 @@
-//http://gtcmt-android-swarmbot.googlecode.com/svn-history/r61/trunk/Demokit2/src/com/google/android/DemoKit/CtView.java
-//imran
+/**
+* Copyright (C) 2013 Imran Akthar (www.imranakthar.com)
+* imran@imranakthar.com
+*/
+
 package com.google.android.DemoKit;
 
 import java.io.File;
